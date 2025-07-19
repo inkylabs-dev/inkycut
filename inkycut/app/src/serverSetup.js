0574884f162720bb44607c1846895992669ff94c
soasme <soasme@gmail.com>
2025-07-19 23:49:35 +1200

    set cors.

--- inkycut/app/src/serverSetup.js
+++ inkycut/app/src/serverSetup.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 export const serverMiddlewareFn = (middlewareConfig) => {
-  // Example of adding extra domains to CORS.
+  middlewareConfig.set('cors', cors({
+    origin: ['https://inkycut.com', 'https://www.inkycut.com', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+  }));
   middlewareConfig.set('express.json', express.json({ limit: '50mb' }));
   return middlewareConfig
 }
