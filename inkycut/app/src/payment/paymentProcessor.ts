24cdc62b3b3b2283d0f6ec0e6f75fc1237ab8f2c
soasme <soasme@gmail.com>
2025-07-22 07:32:45 +1200

    remove lemonsqueezy.

--- inkycut/app/src/payment/paymentProcessor.ts
+++ inkycut/app/src/payment/paymentProcessor.ts
@@ -3,7 +3,6 @@ import type { PaymentsWebhook } from 'wasp/server/api';
 import type { MiddlewareConfigFn } from 'wasp/server';
 import { PrismaClient } from '@prisma/client';
 import { stripePaymentProcessor } from './stripe/paymentProcessor';
-import { lemonSqueezyPaymentProcessor } from './lemonSqueezy/paymentProcessor';
 
 export interface CreateCheckoutSessionArgs {
   userId: string;
@@ -17,16 +16,11 @@ export interface FetchCustomerPortalUrlArgs {
 };
 
 export interface PaymentProcessor {
-  id: 'stripe' | 'lemonsqueezy';
+  id: 'stripe';
   createCheckoutSession: (args: CreateCheckoutSessionArgs) => Promise<{ session: { id: string; url: string }; }>; 
   fetchCustomerPortalUrl: (args: FetchCustomerPortalUrlArgs) => Promise<string | null>;
   webhook: PaymentsWebhook;
   webhookMiddlewareConfigFn: MiddlewareConfigFn;
 }
 
-/**
- * Choose which payment processor you'd like to use, then delete the 
- * other payment processor code that you're not using  from `/src/payment`
- */
-// export const paymentProcessor: PaymentProcessor = lemonSqueezyPaymentProcessor;
 export const paymentProcessor: PaymentProcessor = stripePaymentProcessor;
