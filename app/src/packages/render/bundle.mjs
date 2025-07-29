42352ae3bc4d272092f58e581bb16b2a7db81274
soasme <soasme@gmail.com>
2025-07-30 10:08:18 +1200

    neat: remove bundle progress.

--- app/src/packages/render/bundle.mjs
+++ app/src/packages/render/bundle.mjs
@@ -7,14 +7,8 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
  
-// You only have to create a bundle once, and you may reuse it
-// for multiple renders that you can parametrize using input props.
-const onProgress = (progress) => {
-  console.log(`Webpack bundling progress: ${progress}%`);
-};
 const bundleLocation = await bundle({
     entryPoint: path.resolve(__dirname, './entry.tsx'),
-    onProgress: onProgress,
     publicPath: '/',
     publicDir: '',
     outDir: path.resolve(__dirname, './bundle'),
