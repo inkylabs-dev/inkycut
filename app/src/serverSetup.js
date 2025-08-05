003ae540e62e6d774faf8165247dd3bd4b97b8eb
soasme <soasme@gmail.com>
2025-08-06 08:18:39 +1200

    feat: allow fly.dev domain in cors.

--- app/src/serverSetup.js
+++ app/src/serverSetup.js
@@ -7,7 +7,7 @@ console.log(config.frontendUrl)
 
 export const serverMiddlewareFn = (middlewareConfig) => {
   middlewareConfig.set('cors', cors({
-    origin: [config.frontendUrl, 'https://inkycut.com', 'https://www.inkycut.com'],
+	  origin: [config.frontendUrl, 'https://inkycut.com', 'https://www.inkycut.com', 'https://inkycut-client.fly.dev'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
